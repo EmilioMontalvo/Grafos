8f11e031221e4fd8aed7d61c9ef246afcaa29ba8
EmilioMontalvo <emilio0023@hotmail.com>
2022-03-03 10:04:50 -0500

    revision final

--- src/Negocio/Main.java
+++ src/Negocio/Main.java
@@ -22,6 +22,8 @@ public class Main {
     public static void main(String[] args) {
         JFLaberinto interfaz=new JFLaberinto();
         interfaz.setVisible(true);
+        
+       
     }
     
 }
